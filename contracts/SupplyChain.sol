cf29cfbf49a2caee30544011f0b4507a7f3f2756
Idowu Emehinola <hydeenoble39@gmail.com>
2021-10-10 17:24:48 +0200

    Added declaration for 'State' enum

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -13,6 +13,8 @@ contract SupplyChain {
 
   // <enum State: ForSale, Sold, Shipped, Received>
 
+  enum State { ForSale, Sold, Shipped, Received }
+
   // <struct Item: name, sku, price, state, seller, and buyer>
   
   /* 
