3cae733ff616a3b62d778a933a43f06fc5f2f96d
Idowu Emehinola <hydeenoble39@gmail.com>
2021-10-10 18:18:48 +0200

    Added code to pass all test cases

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -41,6 +41,7 @@ contract SupplyChain {
   event LogShipped(uint sku);
 
   // <LogReceived event: sku arg>
+  event LogReceived(uint sku);
 
 
   /* 
@@ -57,7 +58,7 @@ contract SupplyChain {
   }
 
   modifier verifyCaller (address _address) { 
-    // require (msg.sender == _address); 
+    require (msg.sender == _address); 
     _;
   }
 
@@ -90,8 +91,14 @@ contract SupplyChain {
     require(items[_sku].state == State.Sold);
     _;
   }
-  // modifier shipped(uint _sku) 
-  // modifier received(uint _sku) 
+  modifier shipped(uint _sku) {
+    require(items[_sku].state == State.Shipped);
+    _;
+  }
+  modifier received(uint _sku) {
+    require(items[_sku].state == State.Received);
+    _;
+  }
 
   constructor() public {
     // 1. Set the owner to the transaction sender
@@ -166,7 +173,10 @@ contract SupplyChain {
   //    - the person calling this function is the buyer. 
   // 2. Change the state of the item to received. 
   // 3. Call the event associated with this function!
-  function receiveItem(uint sku) public {}
+  function receiveItem(uint sku) public shipped(sku) verifyCaller(items[sku].buyer){
+    items[sku].state = State.Received;
+    emit LogReceived(sku);
+  }
 
   // Uncomment the following code block. it is needed to run tests
    function fetchItem(uint _sku) public view 
